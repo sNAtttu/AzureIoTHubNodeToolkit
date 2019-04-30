c5d872a22fc9abe018b01301d457d29e7fca794a
sNAttu <snattu91@gmail.com>
2019-04-30 16:33:22 +0300

    Unnecessary interface

--- src/Configuration/iotHubConfiguration.ts
+++ src/Configuration/iotHubConfiguration.ts
@@ -1,5 +1,4 @@
 import configurationJson from "../../config.json";
-import IConfiguration from "./types/configuration";
 export default class IoTHubConfiguration {
   public static getConnectionString() {
     return configurationJson.iotHubConnectionString;
--- src/Configuration/types/configuration.ts
+++
@@ -1,3 +0,0 @@
-export default interface IConfigurationFile {
-  iotHubConnectionString: string;
-}
